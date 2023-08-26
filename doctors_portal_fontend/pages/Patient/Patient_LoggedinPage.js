2961912a781b99afb82f8bcbe32eaa91be415fda
ShohanEleven <shohanmehedi18gmail.com>
2023-08-26 13:02:07 +0600

    routing

--- doctors_portal_fontend/pages/Patient/Patient_LoggedinPage.js
+++ doctors_portal_fontend/pages/Patient/Patient_LoggedinPage.js
@@ -1,31 +1,55 @@
-import Head from 'next/head'; // Import Head to manage the HTML head section
+import FooterForLoggedin from "../Layout/LoggedinFooter"
+import HeaderForLoggedin from "../Layout/LoggedinHeader"
+import NavigationBarLoggedin from "../Layout/LoggedinNavbar"
+import { useState } from 'react';
+import { useRouter } from "next/router";
 
-import FooterForLoggedin from "../Layout/LoggedinFooter";
-import HeaderForLoggedin from "../Layout/LoggedinHeader";
-import Patient_NavBar from "../Layout/Patient_LogNav";
+export default function () {
+  const router = useRouter();
+  const [showButtons, setShowButtons] = useState(false);
 
-export default function LoggedinPage() {
-    return (
-        <>
-          <title>LoggedIn</title>
+  const handlePicClick = () => {
+    setShowButtons(!showButtons);
+  };
 
+  return (
+    <>
+<div className="navbar bg-yellow-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         
-          <Patient_NavBar></Patient_NavBar>
+        <li><button onClick={() => router.push("../Patient/addAppointment")}>Add Appointment</button></li>
+        <li><button onClick={() => router.push("../Patient/deleteAppointment")}>Delete Appointment</button></li>
+        <li><button onClick={() => router.push("../Patient/FeedbackForm")}>Feedback</button></li>
+        <li><button onClick={() => router.push("../Patient/OrderMedicineForm")}>Order Medicine</button></li>
+        <li><button onClick={() => router.push("../Patient/payment")}>Payment</button></li>
+        <li><button onClick={() => router.push("../Patient/searchDoctor")}>Search Doctor</button></li>
+        <li><button onClick={() => router.push("../Patient/test")}>Test</button></li>
+        <li><button onClick={() => router.push("../Patient/update_test")}>Update Test</button></li>
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost normal-case text-xl">Patient Profile</a>
+  </div>
+ 
+</div>
 
 
-          
-           
-       
-          
-    <div align="center" style={{ padding: '20px', border: '1px ' }}>
-      
-      </div>
-            
-            
-              
-    
-    
-          
-          </>
-      )
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
 }
