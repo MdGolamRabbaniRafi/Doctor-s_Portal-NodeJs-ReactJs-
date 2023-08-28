f9345272d9d1e88d5282d88651a6d7d462731fb1
FarisHollow <mirfaris79@gmail.com>
2023-08-29 02:47:54 +0600

    View article

--- doctors_portal_fontend/pages/Doctor/View_article.js
+++ doctors_portal_fontend/pages/Doctor/View_article.js
@@ -5,38 +5,29 @@ import HeaderForLoggedin from '../Layout/LoggedinHeader';
 import FooterForLoggedin from '../Layout/LoggedinFooter';
 import Link from 'next/link';
 import NavigationBarLoggedin from "../Layout/LoggedinNavbar"
-import dynamic from "next/dynamic";
-
-
-const Title = dynamic(()=>import('../Layout/Doctor_Title'),{
-
-  ssr: false,
-
-});
-
 
 export default function Notification() {
   const [notifications, setArticle] = useState([]);
   const [error, setError] = useState('');
   const router = useRouter();
 
-
   const handleBackClick = () => {
     router.push('/');
   };
+  
   useEffect(() => {
     fetchData();
   }, []);
-  
+
   const fetchData = async () => {
     try {
-      const response = await axios.get(`http://localhost:3000/Doctor/viewArticle`)
+      const response = await axios.get(`http://localhost:3000/Doctor/viewArticle`);
       console.log("Backend Response:", response.data);
-  
+
       if (Array.isArray(response.data)) {
         const ArticleData = response.data;
         console.log("ArticleData Data:", ArticleData);
-  
+
         if (ArticleData.length > 0) {
           console.log("setArticle:", ArticleData);
           setArticle(ArticleData);
@@ -52,33 +43,44 @@ export default function Notification() {
       setError('An error occurred. Please try again later.');
     }
   };
-  
-  
 
   return (
+    <div className="bg-gray-100 min-h-screen">
 
-    <div>
-      <Title page ="Articles"></Title>
-
-
-      <h1>Articles</h1>
-      {error && <p>{error}</p>}
-      <ul>
-        {notifications.map(Article => (
-          
-          <li>
-            Name: {Article.name}<br />
-            Link:<Link href={Article.Link}>{Article.Link}</Link><br></br>
-            Date: {Article.Publish_date}<br />
-            Time: {Article.Publish_time}<br />
-            <br></br>
-
-          </li>
-          
-        ))}
-      </ul>
-      <input type="submit" value="Back" onClick={handleBackClick} />
-
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <h1 className="text-3xl font-semibold mb-4">Articles</h1>
+        {notifications.length > 0 && (
+          <p className="text-gray-500 mb-8">
+            <span className="text-lg font-semibold">Total Articles:</span> {notifications.length}
+          </p>
+        )}
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <ul className="space-y-4">
+          {notifications.map((Article, index) => (
+            <li className="border p-4 rounded-md bg-white shadow-md relative flex">
+              <div className="w-7 h-7 bg-blue-500 text-white rounded-full flex items-center justify-center text-xl mr-4">
+                {index + 1}
+              </div>
+              <div className="flex-1">
+                <div className="text-lg font-semibold mb-2">Name: {Article.name}</div>
+                <div className="mb-2">
+                  Link: <Link href={Article.Link}>{Article.Link}</Link>
+                </div>
+              </div>
+              <div className="self-end text-gray-800 ">
+                <p>Date: {Article.Publish_date}</p>
+                <p>Time: {Article.Publish_time}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button
+          onClick={handleBackClick}
+          className="mt-8 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300"
+        >
+          Back
+        </button>
+      </div>
     </div>
   );
 }
